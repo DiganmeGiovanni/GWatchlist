062ef8a86f9946eec82f3a7375a7c11f8779b46b
Giovanni Aguirre <giovanni.fi05@gmail.com>
2016-08-19 20:04:48 -0500

    :art: Jersey implemented

---
+++ src/main/java/me/gwatchlist/rservices/RestRegisterer.java
@@ -0,0 +1,22 @@
+package me.gwatchlist.rservices;
+
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by giovanni on 19/08/16.
+ */
+public class RestRegisterer extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+
+        Set<Class<?>> classes = new HashSet<Class<?>>();
+
+        // Register REST Services
+        classes.add(HelloService.class);
+
+        return classes;
+    }
+}
